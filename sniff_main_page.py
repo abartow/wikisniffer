6f90188f218052b35ec0922f3b7ca7cd495bb447
Andrew Bartow <abartow@mit.edu>
2017-12-14 11:16:34 -0500

    Use Headless Chrome

--- sniff_main_page.py
+++ sniff_main_page.py
@@ -13,26 +13,26 @@ import random
 
 def open_webbrowser_thread_target(url):
 	subprocess.Popen(["osascript", "-e", 'quit app "Google Chrome"'], close_fds=True)
-	time.sleep(3)
-	subprocess.Popen(["open", "-a", "Google Chrome", url], close_fds=True)
+	time.sleep(1)
+	subprocess.Popen(["/Applications/Google Chrome.app/Contents/MacOS/Google Chrome", "--headless", "--disable-gpu", url], close_fds=True)
 
 def sniff_page(page_name):
+	print "Sniffing {}".format(page_name)
 	page_url = "https://en.wikipedia.org/wiki/{}".format(page_name)
 	open_webbrowser_thread = threading.Thread(target=open_webbrowser_thread_target, args=(page_url,))
 	open_webbrowser_thread.start()
 	sniffer = WikipediaMetadataSniffer()
-	sniffer.sniff(15)
-	return sniffer.extract_conversations()
+	sniffer.sniff(5)
+	conversations = sniffer.extract_conversations()
+	print "Sniffed: {}".format(conversations)
+	return conversations
 
-pageviews = WikipediaPageviewAnalyzer("pageviews-20171031-110000")
+pageviews = WikipediaPageviewAnalyzer("pageviews-20171122-150000")
 
 repeat_times = 5
-pages_to_crawl = 1
+num_pages_to_crawl = 1
 
-output_dataset = []
-for unused in range(repeat_times):
-	metadata_mapping = {}
-
+'''
 	pages_crawled = 0
 	i = 24
 	while pages_crawled < pages_to_crawl:
@@ -43,13 +43,23 @@ for unused in range(repeat_times):
 		if page_title == "-" or page_title.startswith("Special:"):
 			continue
 
+		pages_crawled += 1
+'''
+
+output_dataset = []
+for unused in range(repeat_times):
+	metadata_mapping = {}
+
+	pages_to_crawl = ["Darth_Vader", "Harvard_University"]
+
+	for page_title in pages_to_crawl:
 		conversations = sniff_page(page_title)
 		if conversations == {}:
 			# Retry once
 			conversations = sniff_page(page_title)
 		metadata_mapping[page_title] = conversations
 
-		pages_crawled += 1
+
 
 	output_dataset.append(metadata_mapping)
 
